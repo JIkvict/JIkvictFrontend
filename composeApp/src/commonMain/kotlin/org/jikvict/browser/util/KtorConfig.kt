56fc08b5b4470950c341123416c28226e769db50
ikvict07 <antongorobec101@gmail.com>
2025-08-22 01:50:15 +0200

    Bring back content negotiation

--- composeApp/src/commonMain/kotlin/org/jikvict/browser/util/KtorConfig.kt
+++ composeApp/src/commonMain/kotlin/org/jikvict/browser/util/KtorConfig.kt
@@ -2,12 +2,18 @@ package org.jikvict.browser.util
 
 import io.ktor.client.HttpClientConfig
 import io.ktor.client.plugins.api.createClientPlugin
+import io.ktor.client.plugins.contentnegotiation.ContentNegotiation
 import io.ktor.client.request.forms.MultiPartFormDataContent
 import io.ktor.http.HttpHeaders
+import io.ktor.serialization.kotlinx.json.json
 
 fun clientConfig(client: HttpClientConfig<*>) {
     with(client) {
         expectSuccess = true
+
+        install(ContentNegotiation) {
+            json()
+        }
 
         install(
             createClientPlugin("FixMultipartContentType") {
